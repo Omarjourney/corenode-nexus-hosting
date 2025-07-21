195389d1a6c7596e3496496700212b17ab346068
Mr WTF <125758201+Omarjourney@users.noreply.github.com>
2025-07-21 17:31:03 -0400

    Add configurable Bedrock server sliders

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -3,10 +3,14 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 
 import { cn } from "@/lib/utils"
 
+type SliderProps = React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & {
+  color?: "primary" | "secondary" | "tertiary";
+}
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+  SliderProps
+>(({ className, color = "primary", ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -16,9 +20,24 @@ const Slider = React.forwardRef<
     {...props}
   >
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      <SliderPrimitive.Range
+        className={cn("absolute h-full", {
+          "bg-primary": color === "primary",
+          "bg-secondary": color === "secondary",
+          "bg-tertiary": color === "tertiary",
+        })}
+      />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb
+      className={cn(
+        "block h-5 w-5 rounded-full border-2 bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+        {
+          "border-primary": color === "primary",
+          "border-secondary": color === "secondary",
+          "border-tertiary": color === "tertiary",
+        }
+      )}
+    />
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
